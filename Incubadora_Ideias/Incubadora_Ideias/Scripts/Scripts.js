b2ebaeaf40270f5d20d178311540a74fc312563f
Oleksandr Novak <simbol00@gmail.com>
2015-07-06 17:31:02 +0100

    scripts and css

---
+++ Incubadora_Ideias/Incubadora_Ideias/Scripts/Scripts.js
@@ -0,0 +1,27 @@
+﻿//registation photo upload
+
+function readURL(input) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $('#blah').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+}
+
+$(document).on('change', '.btn-file :file', function () {
+    var input = $(this),
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [1, label]);
+});
+
+$(document).ready(function () {
+    $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+    });
+});
