516652669057c3998b1a9536180dc461489ea1fc
Oleksandr Novak <simbol00@gmail.com>
2015-07-06 16:14:15 +0100

    Projeto com base de dados funcional

--- Incubadora_Ideas/Incubadora_Ideas/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Incubadora_Ideas.Global" Language="C#" %>
---
+++ Incubadora_Ideias/Incubadora_Ideias/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Incubadora_Ideias.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
